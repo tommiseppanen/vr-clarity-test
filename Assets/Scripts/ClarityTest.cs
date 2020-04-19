e12d0d8d51c90b11ab64127e022749372a4c3232
Tommi Seppänen <tommi.t.seppanen@gmail.com>
2020-04-19 20:01:38 +0300

    Use standard formula

--- Assets/Scripts/ClarityTest.cs
+++ Assets/Scripts/ClarityTest.cs
@@ -9,21 +9,29 @@ public class ClarityTest : MonoBehaviour
     [SerializeField]
     private GameObject ratingValuePrefab;
 
-    private const float letterMultiplier = 0.023f;
+    private const float standardVision = 5.0f/60.0f;
+    private const float distance = 6.0f;
+    private float standardLetterSize = 1.0f;
+    private const float startHeight = 1.9f;
+    private const float horizontalOffset = -0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
+        standardLetterSize = 2.0f * distance * Mathf.Tan(Mathf.Deg2Rad*(standardVision / 2.0f));
+        var spaceSize = standardLetterSize * 12.5f;
+        
         for (int row = 1; row < 11; row++)
         {
             var visionRating = 0.1f * row;
+            var rowLetterSize = standardLetterSize * (1.0f / visionRating);
             for (int letter = 0; letter < 6; letter++)
             {
-                var newLetter = Instantiate(ePrefab, new Vector3(letter * 0.03f, 1.7f - row * 0.05f, 0), Quaternion.Euler(0, 0, Random.Range(0, 4) * 90));        
-                newLetter.transform.localScale = Vector3.one * letterMultiplier * 0.1f / visionRating;//(row * 0.01f + 0.02f);
+                var newLetter = Instantiate(ePrefab, new Vector3(horizontalOffset+letter * spaceSize, startHeight - row * 1.25f *spaceSize, 0), Quaternion.Euler(0, 0, Random.Range(0, 4) * 90));        
+                newLetter.transform.localScale = Vector3.one * rowLetterSize;
             }
-            var rating = Instantiate(ratingValuePrefab, new Vector3(7 * 0.03f, 1.7f - row * 0.05f, 0), Quaternion.identity);
-            rating.transform.localScale = Vector3.one * letterMultiplier;
+            var rating = Instantiate(ratingValuePrefab, new Vector3(horizontalOffset+7 * spaceSize, startHeight - row * 1.25f * spaceSize, 0), Quaternion.identity);
+            rating.transform.localScale = Vector3.one * standardLetterSize * 5.0f;
             var textMesh = rating.GetComponent(typeof(TextMesh)) as TextMesh;
             textMesh.text = visionRating.ToString("0.0#");
         }
@@ -43,5 +51,12 @@ public class ClarityTest : MonoBehaviour
             UnityEngine.XR.XRSettings.eyeTextureResolutionScale -= 0.25f;
             Debug.Log(UnityEngine.XR.XRSettings.eyeTextureResolutionScale);
         }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Camera.main.transform = new Vector3(0, 0, distance);
+        }
+
+        
     }
 }
