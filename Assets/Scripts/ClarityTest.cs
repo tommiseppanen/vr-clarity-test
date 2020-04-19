089959a8185712654895dc3f95b2a86290fc26d2
Tommi Seppänen <tommi.t.seppanen@gmail.com>
2020-04-19 13:06:45 +0300

    Indicate vision rating

--- Assets/Scripts/ClarityTest.cs
+++ Assets/Scripts/ClarityTest.cs
@@ -6,6 +6,8 @@ public class ClarityTest : MonoBehaviour
 {
     [SerializeField]
     private GameObject ePrefab;
+    [SerializeField]
+    private GameObject ratingValuePrefab;
 
     private const float letterMultiplier = 0.023f;
 
@@ -14,12 +16,16 @@ public class ClarityTest : MonoBehaviour
     {
         for (int row = 1; row < 11; row++)
         {
+            var visionRating = 0.1f * row;
             for (int letter = 0; letter < 6; letter++)
             {
-                var newLetter = Instantiate(ePrefab, new Vector3(letter * 0.03f, 1.7f - row *0.03f, 0), Quaternion.Euler(0, 0, Random.Range(0, 4)*90));
-                var visionRating = 0.1f * row;
-                newLetter.transform.localScale = Vector3.one * letterMultiplier * 0.1f/visionRating;//(row * 0.01f + 0.02f);
+                var newLetter = Instantiate(ePrefab, new Vector3(letter * 0.03f, 1.7f - row * 0.05f, 0), Quaternion.Euler(0, 0, Random.Range(0, 4) * 90));        
+                newLetter.transform.localScale = Vector3.one * letterMultiplier * 0.1f / visionRating;//(row * 0.01f + 0.02f);
             }
+            var rating = Instantiate(ratingValuePrefab, new Vector3(7 * 0.03f, 1.7f - row * 0.05f, 0), Quaternion.identity);
+            rating.transform.localScale = Vector3.one * letterMultiplier;
+            var textMesh = rating.GetComponent(typeof(TextMesh)) as TextMesh;
+            textMesh.text = visionRating.ToString("0.0#");
         }
     }
 
